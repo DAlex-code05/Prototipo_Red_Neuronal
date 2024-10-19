ee17dc2cbcdd74446dd131edeccdcec9c94dee71
DAlex-code05 <dlarez2003@gmail.com>
2024-10-19 14:17:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, jsonify
 import tensorflow as tf
 import cv2
 import os
 import numpy as np
 from PIL import Image
+import base64
+import io
 
 app = Flask(__name__)
 
@@ -59,6 +61,28 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Ruta para la predicción
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    image_data = data['image']
+
+    # Decodificar la imagen
+    image_data = image_data.split(',')[1]  # Eliminar el prefijo "data:image/png;base64,"
+    image = Image.open(io.BytesIO(base64.b64decode(image_data)))
+
+    # Preprocesar la imagen
+    image = image.resize((224, 224))  # Cambia el tamaño según lo que necesite tu modelo
+    img_array = np.array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Realizar la predicción
+    predictions = model.predict(img_array)
+    predicted_class_index = np.argmax(predictions)
+    predicted_class = classes[predicted_class_index]
+
+    return jsonify({'class': predicted_class})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
